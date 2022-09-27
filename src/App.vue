d86f426e8c819858efd4b425f4c97b47bb5752b3
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2022-09-27 17:33:13 +0300

    add textbox component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <script>
 import ComboBox from './components/ComboBox.vue'
+import TextBox from './components/TextBox.vue'
 
 export default {
   components: {
-    ComboBox
+    ComboBox,
+    TextBox
   },
   emits: ['update:modelValue'],
   data() {
     return {
       currencies: ['USD', 'EUR', 'RUB'],
+      fromAmount: 10,
+      toAmount: 20,
       fromCurrency: 'USD',
       toCurrency: 'RUB'
     }
   },
-  watch: {
-    fromCurrency() {
-      console.log(this.fromCurrency)
+  mounted() {
+    
+  },  
+  methods: {
+    changeCurrencies() {
+      const tempAmount = this.fromAmount
+      this.fromAmount = this.toAmount
+      this.toAmount = tempAmount
+
+      const tempCurrency = this.fromCurrency
+      this.fromCurrency = this.toCurrency
+      this.toCurrency = tempCurrency
     }
   }
 }
@@ -23,23 +36,29 @@ export default {
 </script>
 
 <template>
+  <h1>Currency converter</h1>
   <div class="container">
-    <input type="text">
+    <text-box label="Amount" v-model="fromAmount" />
     <combo-box label="From" :items="currencies" v-model="fromCurrency"></combo-box>
-    <button class="change-currencies-button"><img
+    <button class="change-currencies-button" @click="changeCurrencies"><img
         src="https://cdn2.iconfinder.com/data/icons/arrows-set-1/512/27-512.png" alt=""></button>
-    <input type="text">
+    <text-box label="Amount" v-model="toAmount" :isReadonly="true"/>
     <combo-box label="To" :items="currencies" v-model="toCurrency"></combo-box>
   </div>
 </template>
 
 <style scoped>
+h1 {
+  text-align: center;
+  margin: 20% auto 50px;
+  
+}
 .container {
   width: 620px;
-  margin: 30% auto;
+  margin: 0 auto;
   display: grid;
   grid-gap: 1%;
-  grid-template-columns: 1fr 1fr 60px 1fr 1fr;
+  grid-template-columns: 3fr 2fr 60px 3fr 2fr;
 }
 
 .change-currencies-button {
@@ -47,6 +66,9 @@ export default {
   width: 50px;
   padding: 0;
   margin: 35% auto 0;
+  background-color: white;
+  border: 0;
+  cursor: pointer;
 }
 
 .change-currencies-button img {
---
+++ src/components/TextBox.vue
@@ -0,0 +1,41 @@
+<script>
+import { readonly } from 'vue';
+
+export default {
+  name: 'TextBox',
+  props: ['modelValue', 'label', 'isReadonly'],
+  data() {
+    return {
+      content: this.modelValue
+    }
+  },
+  watch: {
+    content() {
+      this.$emit('update:modelValue', this.content)
+    },
+    modelValue() {
+      this.content = this.modelValue
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="textbox">
+    <label for="textbox-input">{{ label }}</label>
+    <input id="textbox-input" type="text" min="0" v-model="content" :readonly="isReadonly"/>
+  </div>
+</template>
+
+<style>
+.textbox {
+  width: 100%;
+}
+
+#textbox-input {
+  width: 100%;
+  height: 46px;
+  padding: 0;
+  font-size: 12pt;
+}
+</style>
