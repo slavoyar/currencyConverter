cd74eb5bfe6364f7fe6d1c89034dedb0a7517c28
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2022-09-27 16:23:06 +0300

    add combobox component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import ComboBox from './components/ComboBox.vue'
+
+export default {
+  components: {
+    ComboBox
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      currencies: ['USD', 'EUR', 'RUB'],
+      fromCurrency: 'USD',
+      toCurrency: 'RUB'
+    }
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <combo-box label="From" :items="currencies" v-model="fromCurrency"></combo-box>
+  <combo-box label="To" :items="currencies" v-model="toCurrency"></combo-box>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/ComboBox.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'ComboBox',
+  props: ['label', 'items', 'modelValue'],
+  data() {
+    return {
+      content: this.modelValue
+    }
+  },
+  watch: {
+    content() {
+      this.$emit('update:modelValue', this.content)
+    },
+    modelValue() {
+      this.content = this.modelValue
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="combobox">
+    <label for="combobox-select">{{ label }}</label>
+    <select id="combobox-select" v-model="content">
+      <option v-for="item of items">{{ item }}</option>
+    </select>
+  </div>
+</template>
+
+<style>
+.combobox {
+  font-size: 14pt;
+  width: 300px;
+}
+
+.combobox-label {
+  padding: 0;
+  margin: 0;
+}
+
+#combobox-select {
+  width: 100%;
+  font-size: 14pt;
+}
+</style>
